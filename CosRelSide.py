99dd0cba46875e322cf598be95ab1a35fd733f3a
soulbreather <kongsamuel@hotmail.dk>
2019-03-14 14:25:19 +0100

    Delete CosRelSide.py

--- CosRelSide.py
+++
@@ -1,6 +0,0 @@
-import math
-
-
-def CosRelSide(side1, side2, vinkelMellemSider):
-    vinkel3 = math.sqrt(side1**2 + side2**2 - 2 * side1 * side2 * math.cos(math.radians(vinkelMellemSider)))
-    return vinkel3
