a6c54a811a17bac82c85c24bcc314a351babb253
soulbreather <kongsamuel@hotmail.dk>
2019-03-14 12:33:39 +0100

    Delete SinRelSide.py

--- SinRelSide.py
+++
@@ -1,6 +0,0 @@
-# Dette program udregner vha. sinusrelationen den manglende vinkel
-import math
-
-
-def SinRelSide(side1, vinkel1, vinkel2):
-    return (side1 * math.sin(math.radians(vinkel2))) / math.sin((math.radians(vinkel1)))
